80fadbe1d7bc30b5d2ac1cf8005ce790627b69fc
Le Zheng <229636060@qq.com>
2020-07-31 17:51:06 +0800

    zll

---
+++ pages/index/index.js
@@ -0,0 +1,22 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    interval: 2000,
+    duration: 500,
+    indicatorDots_pic: true,
+    vertical_pic: false,
+    autoplay_pic: true,
+    interval_pic: 2000,
+    duration_pic: 500
+  },
+ 
+  onLoad: function () {
+
+  }
+})
