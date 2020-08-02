3ff21930125dc1d28021d048ce818567455868ac
229636060@qq.com <229636060@qq.com>
2020-08-02 15:19:27 +0800

    zll

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,7 @@ Page({
     indicatorDots: true,
     vertical: false,
     autoplay: true,
-    interval: 2000,
+    interval: 6000,
     duration: 500,
     indicatorDots_pic: true,
     vertical_pic: false,
@@ -15,7 +15,16 @@ Page({
     interval_pic: 2000,
     duration_pic: 500
   },
- 
+  gotonav:function(e){
+     var e_id = e.currentTarget.dataset.id;
+     switch(e_id){
+      case '2':
+        wx.navigateTo({
+          url:"../school/index"
+        });
+        break;
+     }  
+  },
   onLoad: function () {
 
   }
